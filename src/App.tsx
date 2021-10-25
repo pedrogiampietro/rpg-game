5c17a1aa65b1f639588a1b3ea453361af7830aaf
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-10-25 17:32:43 -0300

    create a hook character.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import * as C from './App.styles';
 import { Character } from './components/Character';
 
+import { useCharacter } from './hooks/useCharacter';
+
 export function App() {
+  const { x, y, moveDown, moveUp, moveLeft, moveRight } = useCharacter();
+
   return (
     <C.Container>
       <C.Map>
-        <Character />
+        <Character x={x} y={y} />
       </C.Map>
     </C.Container>
   );
--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,5 +1,12 @@
 import * as C from './styles';
 
-export function Character() {
-  return <C.Container>...</C.Container>;
+type Props = {
+  x: number;
+  y: number;
+};
+
+export function Character({ x, y }: Props) {
+  const size = 30;
+
+  return <C.Container size={size} left={x * size} top={y * size}></C.Container>;
 }
