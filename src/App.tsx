430642e5b9b960f2775e1a131dacff5473134b42
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-10-25 18:46:32 -0300

    add png character, and change position

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
+import { useEffect } from 'react';
 import * as C from './App.styles';
 import { Character } from './components/Character';
 
 import { useCharacter } from './hooks/useCharacter';
 
 export function App() {
-  const { x, y, moveDown, moveUp, moveLeft, moveRight } = useCharacter();
+  const { x, y, side, moveDown, moveUp, moveLeft, moveRight } = useCharacter();
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        moveLeft();
+        break;
+
+      case 'KeyW':
+      case 'ArrowUp':
+        moveUp();
+        break;
+
+      case 'KeyD':
+      case 'ArrowRight':
+        moveRight();
+        break;
+
+      case 'KeyS':
+      case 'ArrowDown':
+        moveDown();
+        break;
+    }
+  };
 
   return (
     <C.Container>
       <C.Map>
-        <Character x={x} y={y} />
+        <Character x={x} y={y} side={side} />
       </C.Map>
     </C.Container>
   );
--- src/components/Character/styles.ts
+++ src/components/Character/styles.ts
@@ -4,11 +4,13 @@ export const Container = styled.div<{
   size: number;
   left: number;
   top: number;
+  sidesPos: number;
 }>`
   width: ${props => props.size}px;
   height: ${props => props.size}px;
   position: absolute;
   left: ${props => props.left}px;
   top: ${props => props.top}px;
-  background-color: red;
+  background-image: url('/assets/char.png');
+  background-position: 0px ${props => props.sidesPos}px;
 `;
